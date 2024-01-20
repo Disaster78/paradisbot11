e5981fda7e80103647d988e9a81621a6ecbb79a9
Disaster78 <88614078+Disaster78@users.noreply.github.com>
2024-01-20 22:57:50 +0530

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -220,4 +220,4 @@ class Moderation(commands.Cog, name="Moderation"):
         await ctx.send(embed=embed)
 
 def setup(bot):
-    bot.add_cog(Moderation(bot))
+    await bot.add_cog(Moderation(bot))
